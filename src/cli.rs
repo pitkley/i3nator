10c1cd46a852a170c48dded46dbe064f4f5ebaea
Pit Kleyersburg <pitkley@googlemail.com>
2017-05-16 18:19:57 +0200

    Add remaining subcommands to usage

--- src/cli.rs
+++ src/cli.rs
@@ -29,7 +29,28 @@ pub fn cli() -> App<'static, 'static> {
                     AppSettings::InferSubcommands,
                     AppSettings::SubcommandRequiredElseHelp,
                     AppSettings::VersionlessSubcommands,])
+        .subcommand(SubCommand::with_name("copy")
+                    .arg(Arg::with_name("EXISTING").required(true))
+                    .arg(Arg::with_name("NEW").required(true)))
+        .subcommand(SubCommand::with_name("delete")
+                    .arg(Arg::with_name("PROJECT").multiple(true).required(true)))
         .subcommand(SubCommand::with_name("edit")
                         .alias("open")
                         .arg(Arg::with_name("PROJECT").required(true)))
+        // TODO: decide if we want to add `implode?`
+        .subcommand(SubCommand::with_name("list"))
+        .subcommand(SubCommand::with_name("local")
+                    .arg(Arg::with_name("file")
+                         .short("f")
+                         .long("file")
+                         .value_name("FILE")
+                         .default_value("tmuxinator.yml")))
+        .subcommand(SubCommand::with_name("new")
+                    .arg(Arg::with_name("PROJECT")
+                         .required(true)))
+        .subcommand(SubCommand::with_name("start")
+                    .alias("run"))
+    // TODO: determine if we can implement `stop`.
+    // This would probably require keeping track of PIDs and workspaces and such, so my
+    // immediate thought is "no".
 }
