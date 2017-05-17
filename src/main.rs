2be654225c35b04d734ebd523731f3656c3debec
Pit Kleyersburg <pitkley@googlemail.com>
2017-05-17 20:54:15 +0200

    Implement `command_{copy,delete,list}`

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,81 @@
 extern crate clap;
 #[macro_use]
 extern crate error_chain;
+#[macro_use]
+extern crate lazy_static;
+extern crate glob;
+extern crate xdg;
 
 mod cli;
 mod errors {
-    error_chain!{}
+    error_chain! {
+        foreign_links {
+            GlobError(::glob::PatternError);
+            IoError(::std::io::Error);
+        }
+
+        errors {
+            ProjectExists(t: String) {
+                description("project already exists")
+                display("project already exists: '{}'", t)
+            }
+
+            UnknownProject(t: String) {
+                description("project is unknown")
+                display("project is unknown: '{}'", t)
+            }
+        }
+    }
 }
 
 use clap::ArgMatches;
 use errors::*;
+use glob::glob;
+use std::fs;
+use std::path::PathBuf;
 
-fn command_copy(_matches: &ArgMatches<'static>) -> Result<()> {
-    unimplemented!()
+lazy_static! {
+    static ref XDG_DIRS: xdg::BaseDirectories =
+        xdg::BaseDirectories::with_prefix(crate_name!()).expect("couldn't get XDG base directory");
+    static ref PROJECTS_PATH: PathBuf =
+        XDG_DIRS.create_config_directory("projects").expect("couldn't create projects directory");
 }
 
-fn command_delete(_matches: &ArgMatches<'static>) -> Result<()> {
-    unimplemented!()
+fn command_copy(matches: &ArgMatches<'static>) -> Result<()> {
+    // `EXISTING` and `NEW` should not be empty, clap ensures this.
+    let existing_project_name = matches.value_of("EXISTING").unwrap();
+    let new_project_name = matches.value_of("NEW").unwrap();
+
+    let existing_project_path = PROJECTS_PATH.join(format!("{}.toml", existing_project_name));
+    let new_project_path = PROJECTS_PATH.join(format!("{}.toml", new_project_name));
+
+    if existing_project_path.exists() && existing_project_path.is_file() {
+        if new_project_path.exists() {
+            Err(ErrorKind::ProjectExists(new_project_name.to_owned()).into())
+        } else {
+            fs::copy(existing_project_path, new_project_path)?;
+            println!("Copied existing project '{}' to new project '{}'",
+                     existing_project_name,
+                     new_project_name);
+            Ok(())
+        }
+    } else {
+        Err(ErrorKind::UnknownProject(existing_project_name.to_owned()).into())
+    }
+}
+
+fn command_delete(matches: &ArgMatches<'static>) -> Result<()> {
+    // `PROJECT` should not be empty, clap ensures this.
+    let project_name = matches.value_of("PROJECT").unwrap();
+    let project_path = PROJECTS_PATH.join(format!("{}.toml", project_name));
+
+    if project_path.exists() && project_path.is_file() {
+        fs::remove_file(project_path)?;
+        println!("Deleted project '{}'", project_name);
+        Ok(())
+    } else {
+        Err(ErrorKind::UnknownProject(project_name.to_owned()).into())
+    }
 }
 
 fn command_edit(_matches: &ArgMatches<'static>) -> Result<()> {
@@ -32,7 +92,25 @@ fn command_edit(_matches: &ArgMatches<'static>) -> Result<()> {
 }
 
 fn command_list(_matches: &ArgMatches<'static>) -> Result<()> {
-    unimplemented!()
+    let project_path = PROJECTS_PATH.join("*.toml");
+    let project_path = project_path
+        .to_str()
+        .ok_or_else(|| Error::from("couldn't get projects"))?;
+
+    println!("i3nator projects:");
+
+    for entry in glob(project_path)? {
+        if let Some(project) = entry
+               .ok()
+               .and_then(|pathbuf| {
+                             let stem = pathbuf.file_stem();
+                             stem.and_then(|filestem| filestem.to_str().map(|s| s.to_owned()))
+                         }) {
+            println!("  {}", project);
+        }
+    }
+
+    Ok(())
 }
 
 fn command_local(_matches: &ArgMatches<'static>) -> Result<()> {
