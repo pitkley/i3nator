4c439e102effe0683677f0ef9e160abc7dd9de6a
Pit Kleyersburg <pitkley@googlemail.com>
2017-05-19 16:01:56 +0200

    Add documentation to public API

--- src/errors.rs
+++ src/errors.rs
@@ -6,51 +6,83 @@
 // option. This file may not be copied, modified or distributed
 // except according to those terms.
 
+//! Errors, using [`error-chain`][error-chain].
+//!
+//! [error-chain]: https://crates.io/crates/error-chain
+
 error_chain! {
     foreign_links {
-        I3EstablishError(::i3ipc::EstablishError);
-        I3MessageError(::i3ipc::MessageError);
-        IoError(::std::io::Error);
-        Utf8Error(::std::str::Utf8Error);
-        TomlError(::toml::de::Error);
+        I3EstablishError(::i3ipc::EstablishError)
+            #[doc = "Error caused by `i3ipc`, on establishing a connection."];
+
+        I3MessageError(::i3ipc::MessageError)
+            #[doc = "Error caused by `i3ipc`, on sending a message."];
+
+        IoError(::std::io::Error)
+            #[doc = "Error mapping to `std::io::Error`."];
+
+        Utf8Error(::std::str::Utf8Error)
+            #[doc = "Error mapping to `std::str::Utf8Error`."];
+
+        TomlError(::toml::de::Error)
+            #[doc = "Error caused by `toml`, on deserializing using Serde."];
     }
 
     errors {
+        /// An error that can occur when splitting a string into a
+        /// [`ApplicationCommand`][struct-ApplicationCommand].
+        ///
+        /// [struct-ApplicationCommand]: ../types/struct.ApplicationCommand.html
         CommandSplittingFailed(t: String) {
             description("command splitting failed")
             display("command splitting failed: '{}'", t)
         }
 
+        /// An error that occurs when the default editor is not specified.
+        ///
+        /// One of the environment variables `$VISUAL` or `$EDITOR` has to be set.
         EditorNotFound {
             description("cannot find an editor")
             display("cannot find an editor. Please specify $VISUAL or $EDITOR")
         }
 
+        /// An error that occurs when a `Path` (i.e. `OsStr`) cannot be converted to UTF8.
         InvalidUtF8Path(t: String) {
             description("path is invalid UTF8")
             display("path is invalid UTF8: '{}'", t)
         }
 
+        /// An error that occurs if a specified path does not exist.
         PathDoesntExist(t: String) {
             description("path doesn't exist")
             display("path doesn't exist: '{}'", t)
         }
 
+        /// An error that occurs if both [`layout`][structfield-General-layout] and
+        /// [`layout_path`][structfield-General-layout_path] of [`General`][struct-General]
+        /// have not been specified.
+        ///
+        /// [struct-General]: ../types/struct.General.html
+        /// [structfield-General-layout]: ../types/struct.General.html#structfield.layout
+        /// [structfield-General-layout_path]: ../types/struct.General.html#structfield.layout_path
         LayoutNotSpecified {
             description("layout and not specified")
             display("both `layout` and `layout_path` not specified")
         }
 
+        /// An error that occurs if no project exists.
         NoProjectExist {
             description("no projects exist")
             display("no projects exist. Feel free to create one")
         }
 
+        /// An error that occurs if a project under the same name already exists.
         ProjectExists(t: String) {
             description("project already exists")
             display("project already exists: '{}'", t)
         }
 
+        /// An error that occurs if a project does not exist under a specified name.
         UnknownProject(t: String) {
             description("project is unknown")
             display("project is unknown: '{}'", t)
--- src/types.rs
+++ src/types.rs
@@ -6,6 +6,35 @@
 // option. This file may not be copied, modified or distributed
 // except according to those terms.
 
+//! The types in this module make up the structure of the project configuration files.
+//!
+//! # Example
+//!
+//! The following is an examplary TOML configuration, which will be parsed into this modules types.
+//!
+//! ```toml
+//! # i3nator project
+//!
+//! # General configuration items
+//! [general]
+//! # Working directory to use
+//! working_directory = "/path/to/my/working/directory"
+//!
+//! # Name of the workspace the layout should be applied to
+//! workspace = "1"
+//!
+//! # Path to your layout-file
+//! layout_path = "/path/to/my/layout.json"
+//!
+//! # Alternatively, you can include the JSON-contents of the layout directly:
+//! # layout = "{ ... }"
+//!
+//! # List of applications to start
+//! [[applications]]
+//! command = "mycommand --with 'multiple args'"
+//! working_directory = "/path/to/a/different/working/directory"
+//! ```
+
 use deserializers::*;
 use serde::de;
 use serde::de::{Deserialize, Deserializer};
@@ -14,35 +43,113 @@ use std::path::PathBuf;
 
 #[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
+/// This is the parent type defining the complete project configuration used by i3nator.
 pub struct Config {
+    /// The general configuration section.
+    ///
+    /// This section defines how a project behaves in general.
     pub general: General,
+
+    /// The applications configuration list.
+    ///
+    /// This list defines what applications to start and how to start them.
     pub applications: Vec<Application>,
 }
 
 #[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
+/// The general configuration section.
+///
+/// This section defines how a project behaves in general.
 pub struct General {
+    /// The working directory defines in which directory-context the applications should be
+    /// launched in.
     pub working_directory: Option<PathBuf>,
+
+    /// If the workspace is `Some`, `i3` will be instructed to open the layout on the specified
+    /// workspace. If it is `None`, `i3` will use the currently focused workspace.
     pub workspace: Option<String>,
+
+    /// The layout to append to a workspace, given as the quasi-JSON `i3-save-tree` returns and
+    /// [`append_layout`][append-layout] expects.
+    ///
+    /// [append-layout]: https://i3wm.org/docs/layout-saving.html#_append_layout_command
     pub layout: Option<String>,
+
+    /// A file-path containing a quasi-JSON as returned by `i3-save-tree` and expected by
+    /// [`append_layout`][append-layout].
+    ///
+    /// [append-layout]: https://i3wm.org/docs/layout-saving.html#_append_layout_command
     pub layout_path: Option<PathBuf>,
 }
 
 #[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
+/// The applications configuration.
+///
+/// This configuration defines how to start an applications and what potential commands to execute
+/// in them.
 pub struct Application {
+    /// The command used for starting an application.
+    ///
+    /// See [`ApplicationCommand`](struct.ApplicationCommand.html).
     pub command: ApplicationCommand,
+
+    /// The working directory defines in which directory-context the applications should be
+    /// launched in.
+    ///
+    /// This overrides [`general.working_directory`][general-working_directory].
+    ///
+    /// [general-working_directory]: struct.General.html#structfield.working_directory
     pub working_directory: Option<PathBuf>,
+
     #[serde(default, deserialize_with="option_string_or_seq_string")]
+    /// *WIP:* List of strings to input into the started application
     pub text: Option<Vec<String>>,
     #[serde(default="default_text_return")]
+    /// *WIP:* Wether to simulate a `Return` after every string entered from `text`.
     pub text_return: bool,
+    /// *WIP:* Specific keys to input into the started application (such as hotkeys).
     pub keys: Option<Vec<String>>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
+/// The command used for starting an application.
+///
+/// # Example
+///
+/// This struct can be deserialized (from TOML) either from a string or a sequence of strings. The
+/// following are equivalent:
+///
+/// ```toml
+/// command = "myprogram --with 'multiple args'"
+/// command = ["myprogram, "--with", "multiple args"]
+/// ```
+///
+/// ```rust
+/// # extern crate i3nator;
+/// # extern crate toml;
+/// # use i3nator::types::*;
+/// # fn main() {
+/// let string: ApplicationCommand = toml::from_str::<Application>(r#"
+///         command = "myprogram --with 'multiple args'"
+///     "#).unwrap().command;
+/// let sequence_of_strings: ApplicationCommand = toml::from_str::<Application>(r#"
+///         command = ["myprogram", "--with", "multiple args"]
+///     "#).unwrap().command;
+///
+/// assert_eq!(string, sequence_of_strings);
+/// assert_eq!(string.program, "myprogram");
+/// assert_eq!(string.args, Some(vec!["--with".to_owned(), "multiple args".to_owned()]));
+/// # }
+/// ```
+///
+/// A string will be split up into separate args, honoring single- and double-quoted elements.
 pub struct ApplicationCommand {
+    /// The executable to start.
     pub program: String,
+
+    /// A list of arguments to pass to the executable.
     pub args: Option<Vec<String>>,
 }
 
